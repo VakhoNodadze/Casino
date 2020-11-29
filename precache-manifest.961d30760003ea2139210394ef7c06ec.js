8619e9434298bc6a04336f97c1bcf04384abdc22
VakhoNodadze <vaxonodadze@gmail.com>
2020-11-30 01:47:06 +0400

    Updates

---
+++ precache-manifest.961d30760003ea2139210394ef7c06ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9c0401381d6cfbc7abcaed3fab0bae",
+    "url": "/Casino/index.html"
+  },
+  {
+    "revision": "975e5705ed88745f4886",
+    "url": "/Casino/static/css/main.16f48798.chunk.css"
+  },
+  {
+    "revision": "07fd7d2be256c08dbbc8",
+    "url": "/Casino/static/js/2.583baf62.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Casino/static/js/2.583baf62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975e5705ed88745f4886",
+    "url": "/Casino/static/js/main.09e972ec.chunk.js"
+  },
+  {
+    "revision": "ad0981aa338e7f3a77cf",
+    "url": "/Casino/static/js/runtime-main.59b9c451.js"
+  },
+  {
+    "revision": "bc900238f1d4b06a85a1edc7701e421f",
+    "url": "/Casino/static/media/xmas.bc900238.png"
+  }
+]);
